acc96698f54fdb5f27fa48e0cc46bda71553df8b
Minh Tri Ha <minhtrihaa@gmail.com>
2023-06-01 15:39:08 +0200

    Fix error from navLink

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './layouts/rootLayout'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './layouts/rootLayout';
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    children: [],
   },
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- client/src/routes/nav.jsx
+++ client/src/routes/nav.jsx
@@ -3,15 +3,19 @@ import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className="navlink">
-      <NavLink to="/home" activeClassName="active">
-        Home
-      </NavLink>
-      <NavLink to="/about" activeClassName="active">
-        About
-      </NavLink>
-      <NavLink to="/contact" activeClassName="active">
-        Contact
-      </NavLink>
+      <ul>
+        <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+
+        <NavLink to="">
+          <li>About</li>
+        </NavLink>
+
+        <NavLink to="">
+          <li>Test</li>
+        </NavLink>
+      </ul>
     </div>
   )
 }
