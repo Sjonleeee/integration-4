c9d8396f887e88d7f7bd78d778d1ce6bfcc7c7b8
sennedepessemier <senne.de.pessemier@student.howest.be>
2023-06-07 11:57:28 +0200

    test

--- client/src/js/utils/graphql.js
+++ client/src/js/utils/graphql.js
@@ -2,7 +2,7 @@ const BASE_URL = import.meta.env.VITE_API_ENDPOINT || "";
 console.log(BASE_URL);
 
 export const graphQLRequest = async (query, variables = {}) => {
-  const result = await fetch(`${BASE_URL}/api`, {
+  const result = await fetch(${BASE_URL}/api, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
