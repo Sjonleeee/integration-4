214a9c0592bc9ad03ad6ec53826fc36871e49843
sennedepessemier <senne.de.pessemier@student.howest.be>
2023-06-17 13:06:52 +0200

    Revert "let's try"
    This reverts commit 7b226ba52e29118c328538784c7bf8aeb1c089e7.

--- client/src/js/utils/graphql.js
+++ client/src/js/utils/graphql.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_ENDPOINT || "";
+const BASE_URL = "https://integration.minhtriha.com";
 
 console.log(BASE_URL);
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5173",
+        target: "https://integration.minhtriha.com",
         changeOrigin: true,
         secure: false,
       },
